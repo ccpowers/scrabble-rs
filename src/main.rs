3949bf16434bf0981eefda0592c86b1d0fa1c7a1
ccpowers <katypm@gmail.com>
2023-06-04 07:51:27 -0400

    cleaned up warnings in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use game::{game::{ScrabbleGame, generate_scrabble_for_one}, tile_bag::{ExchangeTiles, Tile}};
+use game::{game::{ScrabbleGame, generate_scrabble_for_one}};
 use crate::gui::gui::{generate_scrabble_gui};
-use log::{debug, error, info, trace, warn};
+use log::{info};
 use log4rs;
 
 pub mod game;
@@ -9,16 +9,9 @@ pub mod gui;
 fn main() {
     // initialize logs
     log4rs::init_file("logging.yaml", Default::default()).unwrap();
-    info!("hi");
+    info!("Starting scrabble game");
     //create and initilize the game
-    let mut scrabble_game: ScrabbleGame = generate_scrabble_for_one();
-
-    // create some callbacks on scrabble game
-    let exchange_cb = |tiles: [Option<Tile>;7] | -> [Option<Tile>; 7] {
-        return scrabble_game.tile_bag.exchange_tiles(tiles);
-    };
-
-    
+    let scrabble_game: ScrabbleGame = generate_scrabble_for_one();
     let mut scrabble_gui = generate_scrabble_gui(scrabble_game);
     scrabble_gui.run();
 
